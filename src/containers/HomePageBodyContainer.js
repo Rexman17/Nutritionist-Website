51f04bebc8097d36b2cff018a86a321cacd00647
Ali Reubenstone <a.stone1794@gmail.com>
2019-08-09 15:36:26 -0400

    Add pic of linda to homepage

--- src/containers/HomePageBodyContainer.js
+++ src/containers/HomePageBodyContainer.js
@@ -5,7 +5,10 @@ const HomePageBodyContainer = () => {
 
     return (
       <div>
-        HomePageBodyContainer
+        <div id="homepage-quote">
+          My weight loss program is unique because it is not a diet but a way of life.  It is a lifestyle change which promotes the development of a positive self image along with a plan for healthy eating. 
+        </div>
+        <img id="homepage-pic" src="./assets/linda-home-pic.jpeg" alt="Linda Fiveson" />
       </div>
     )
 
