6df03a60a14478b563b292481559407489de434a
Ali Reubenstone <a.stone1794@gmail.com>
2019-08-09 20:58:06 -0400

    done w mobile version of splash page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header'
 import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 import HomePageBodyContainer from './containers/HomePageBodyContainer'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Header/>
       <NavBar/>
       <HomePageBodyContainer/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => {
+
+  return (
+    <footer>
+      <div id="li-weight-counseling">
+        LONG ISLAND WEIGHT COUNSELING, INC.
+      </div>
+      <div id="footer-address-details">
+        633 JERICHO TURNPIKE
+        SYOSSET, N.Y. 11791
+        <p>516-496-2300</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
