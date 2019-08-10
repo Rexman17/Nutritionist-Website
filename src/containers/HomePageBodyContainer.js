443e805b0f48d233c88a7621f5ca5646d0b7e4e5
Ali Reubenstone <a.stone1794@gmail.com>
2019-08-09 20:34:28 -0400

    Add quote and css arrow icon

--- src/containers/HomePageBodyContainer.js
+++ src/containers/HomePageBodyContainer.js
@@ -6,7 +6,10 @@ const HomePageBodyContainer = () => {
     return (
       <div>
         <div id="homepage-quote">
-          My weight loss program is unique because it is not a diet but a way of life.  It is a lifestyle change which promotes the development of a positive self image along with a plan for healthy eating. 
+          "My weight loss program is unique because <span>it is not a diet but a way of life.</span>  It is a lifestyle change which promotes the development of a positive self image along with a plan for healthy eating."
+          <div id="learn-more-link">Learn More
+            <i id="arrow-icon" class="fa fa-chevron-circle-right"></i>
+          </div>
         </div>
         <img id="homepage-pic" src="./assets/linda-home-pic.jpeg" alt="Linda Fiveson" />
       </div>
